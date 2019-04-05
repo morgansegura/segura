5fb7a66c98da73045b13e970d4b8da5172acf215
Morgan Segura <morgansegura@gmail.com>
2019-04-05 13:24:42 -0700

    Basic setup

--- src/templates/home-page.js
+++ src/templates/home-page.js
@@ -1,26 +1,21 @@
-import React from 'react'
+// import React from 'react'
 
-const HomePageTemplate = ({ data: { prismicHomepage } }) => {
-  const { data } = prismicHomepage
-  // console.log(prismicHomepage)
+// const HomePageTemplate = ({ data: { prismicHomepage } }) => {
+//   const { data } = prismicHomepage
+//   // console.log(prismicHomepage)
+//   return (
+//     <React.Fragment>
+//       <div className="container content__wrapper content__wrapper__with-sidebar">
+//         <div className="row">
+//           <aside className="col-12 col-md-4" />
+//           <section className="col-12 col-md-8">
+//             <h1>{data.title.text}</h1>
+//             <div dangerouslySetInnerHTML={{ __html: data.content.html }} />
+//           </section>
+//         </div>
+//       </div>
+//     </React.Fragment>
+//   )
+// }
 
-  for (let i = 1; i <= 6; i++) {
-    console.log('Isaiah')
-  }
-
-  return (
-    <React.Fragment>
-      <div className="container content__wrapper content__wrapper__with-sidebar">
-        <div className="row">
-          <aside className="col-12 col-md-4" />
-          <section className="col-12 col-md-8">
-            <h1>{data.title.text}</h1>
-            <div dangerouslySetInnerHTML={{ __html: data.content.html }} />
-          </section>
-        </div>
-      </div>
-    </React.Fragment>
-  )
-}
-
-export default HomePageTemplate
+// export default HomePageTemplate

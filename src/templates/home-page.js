bb8c4bf60ffce7e4e731549c189b2d3f93b09995
Morgan Segura <morgansegura@gmail.com>
2019-04-05 13:29:02 -0700

    Rebuild platform

--- src/templates/home-page.js
+++ src/templates/home-page.js
@@ -1,21 +1,18 @@
-// import React from 'react'
+import React from 'react'
 
-// const HomePageTemplate = ({ data: { prismicHomepage } }) => {
-//   const { data } = prismicHomepage
-//   // console.log(prismicHomepage)
-//   return (
-//     <React.Fragment>
-//       <div className="container content__wrapper content__wrapper__with-sidebar">
-//         <div className="row">
-//           <aside className="col-12 col-md-4" />
-//           <section className="col-12 col-md-8">
-//             <h1>{data.title.text}</h1>
-//             <div dangerouslySetInnerHTML={{ __html: data.content.html }} />
-//           </section>
-//         </div>
-//       </div>
-//     </React.Fragment>
-//   )
-// }
+const HomePageTemplate = () => {
+  // const { data } = prismicHomepage
+  // console.log(prismicHomepage)
+  return (
+    <React.Fragment>
+      <div className="container content__wrapper content__wrapper__with-sidebar">
+        <div className="row">
+          <aside className="col-12 col-md-4" />
+          <section className="col-12 col-md-8" />
+        </div>
+      </div>
+    </React.Fragment>
+  )
+}
 
-// export default HomePageTemplate
+export default HomePageTemplate
